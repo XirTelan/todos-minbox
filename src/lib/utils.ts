143189dadaf60a43e87517288b9fe12e2c4994d9
Xir <prssergey@gmail.com>
2025-04-09 18:50:42 +0300

    feat: localStorage save and load

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,32 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { TodosSlice } from "@/types";
+import { Middleware } from "@reduxjs/toolkit";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const loadTodosFromLocalStorage = () => {
+  const defaultState: TodosSlice = {
+    todos: [],
+  };
+  try {
+    const todos = localStorage.getItem("todos");
+    if (todos) defaultState.todos = JSON.parse(todos);
+    return defaultState;
+  } catch (error) {
+    return defaultState;
+  }
+};
+
+const saveTodosToLocalStorage: Middleware = (store) => (next) => (action) => {
+  const result = next(action);
+
+  const slice = store.getState().todosSlice;
+  localStorage.setItem("todos", JSON.stringify(slice.todos));
+
+  return result;
+};
+
+export default saveTodosToLocalStorage;
--- src/store/reduxStore.ts
+++ src/store/reduxStore.ts
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosSlice from "./slices/todosSlice";
+import saveTodosToLocalStorage from "@/lib/utils";
 
 export const store = configureStore({
   reducer: {
     todosSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(saveTodosToLocalStorage),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
